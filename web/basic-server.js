33c33f8c9123aa83d84df6bdb6640d3d973e266a
hanyen <me@hanyen.com>
2017-01-11 14:28:11 -0800

    Setting up the environment

--- web/basic-server.js
+++ web/basic-server.js
@@ -10,10 +10,32 @@ var port = 8080;
 var ip = '127.0.0.1';
 var server = http.createServer(handler.handleRequest);
 
-if (module.parent) {
-  module.exports = server;
-} else {
-  server.listen(port, ip);
-  console.log('Listening on http://' + ip + ':' + port);
-}
+//2. set router
+var router = {
+  '/web-historian': handler.requestHandler
+  // ...
+};
 
+//3. set server
+var server = http.createServer( function(req, res) {
+  console.log('Serving request type ' + req.method + ' for url ' + req.url);
+
+  var route = router[url.parse(req.url).pathname];
+  if (route) {
+    route(req, res);
+  } else {
+    utils.sendResponse(res, '', 404);
+  }
+});
+
+console.log('Listening on http://' + ip + ':' + port);
+server.listen(port, ip);
+
+// if (module.parent) {
+//   module.exports = server;
+// } else {
+//   server.listen(port, ip);
+//   console.log('Listening on http://' + ip + ':' + port);
+// }
+
+console.log(url);
