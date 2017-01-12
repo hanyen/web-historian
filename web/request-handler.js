fadd11c250c304991879cffee673c23d78cc4d43
hanyen <me@hanyen.com>
2017-01-11 16:26:47 -0800

    Passed the first test (render index.html) and wrote pseudocode for htmlfether.js

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,18 +4,15 @@ var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  fs.readFile('./public/index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html', 'Content-length': data.length});
-    res.write(data, function(err) {
-      if (err) {
-        console.log('request-handler.js: could not render index.html to screen');
-      } else {
-        console.log('request-handler.js: render successful');
-        // console.log(res);
-        // console.log(data);
-        res.end();
-      }
-    });
+  //readFile - needs the ABSOLUTE PATHFILE for 'index.html'
+  fs.readFile('/Users/student/Desktop/hrsf53-web-historian/web/public/index.html', function(err, data) {
+    //res.writeHead(200, {'Content-Type': 'text/html', 'Content-length': data.length});
+    if (err) {
+      console.log('request-handler.js: could not render index.html to screen');
+    } else {
+      res.end(data);
+    }
+    
   });
   
   // res.end(archive.paths.list);
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,16 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+//use fs to read the site.txt 
+//check if the user-inputted website with status = 'done' is found in the 'sites.txt' file
+  //yes - 
+    //check the archive folder
+    //fs.readFile (ebay)
+      //render or show the contents
+  //no - status = 'unfetched'
+    //change status to 'fetched'
+    //workers/htmlfetcher.js to fetch the website
+    //(modularize - use helpers/archive-helpers.js) to download the html file
+      // inside the achive-helpers method callback, write HTML to storage ('/archives')
+      // change status to 'done'
+
